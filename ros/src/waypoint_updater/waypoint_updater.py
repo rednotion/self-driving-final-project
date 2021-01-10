81810c8939c24f042195619a75a52a0955f00a83
elizabeth-dsaid <elizabeth@dsaid.gov.sg>
2021-01-10 14:43:22 +0800

    waypoint updater typo

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -95,10 +95,10 @@ class WaypointUpdater(object):
         closest_idx = self.get_closest_waypoint_idx()
         farthest_idx = closest_idx + LOOKAHEAD_WPS
         base_waypoints = self.base_waypoints.waypoints[closest_idx:farthest_idx]
-        if self.stopline_wp_idx == -1 o (self.stopline_wp_idx >= farthest_idx):
-          lane.waypoints = base_waypoints
+        if (self.stopline_wp_idx == -1) or (self.stopline_wp_idx >= farthest_idx):
+            lane.waypoints = base_waypoints
         else:
-          lane.waypoints = self.decelerate_waypoints(base_waypoints, closest_idx)
+            lane.waypoints = self.decelerate_waypoints(base_waypoints, closest_idx)
 
         return lane
 
